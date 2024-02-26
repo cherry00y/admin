ee8d282797bb6266d900172a5dc6986dd9a55e68
cherry00y <anuthida.w64@rsu.ac.th>
2024-02-26 17:03:03 +0700

    .

--- src/Disease/disease.js
+++ src/Disease/disease.js
@@ -18,21 +18,6 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { lightBlue, red } from '@mui/material/colors';
 
 
-
-
-// function createData(name, calories, fat, carbs, protein) {
-//     return { name, calories, fat, carbs, protein };
-//   }
-  
-//   const rows = [
-//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//     createData('Eclair', 262, 16.0, 24, 6.0),
-//     createData('Cupcake', 305, 3.7, 67, 4.3),
-//     createData('Gingerbread', 356, 16.0, 49, 3.9),];
-
-
-
 export default function SimpleContainer() {
     const [items, setItems] = useState([]);
 
